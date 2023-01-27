fa7f3bf04a9c21e740ba56426703e306bc6f8519
Adam Sobotka <vorcigernix@gmail.com>
2023-01-27 20:45:51 +0100

    it does something

--- src/components/editor/Create.svelte
+++ src/components/editor/Create.svelte
@@ -1,65 +1,90 @@
 <script lang="ts">
 	import { cmsStore } from "$routes/gallery/stores";
-	import { handleFileInput } from "$routes/gallery/lib/gallery";
+	import {
+		uploadImageToWNFS,
+		uploadDocumentToWNFS,
+	} from "$routes/gallery/lib/gallery";
+	import type { ContentDoc } from "$routes/gallery/lib/gallery";
 
 	// Handle files uploaded directly through the file input
 	let files: FileList;
+	let preview;
+	let imageResult;
+	let contentHeader, contentText;
+
 	$: if (files) {
-		handleFileInput(files);
+		imageResult = uploadImg(files[0]);
 	}
 
-	import { onMount } from "svelte";
+	async function uploadImg(file: File) {
+		return await uploadImageToWNFS(file);
+	}
 
-	let editor;
+	async function uploadDoc() {
+		console.log(contentHeader, contentText);
+		const doc: ContentDoc = {
+			image: imageResult,
+			header: contentHeader,
+			content: contentText,
+		};
 
-	export let toolbarOptions = [
-		[{ header: 1 }, { header: 2 }, "blockquote", "link", "image", "video"],
-		["bold", "italic", "underline", "strike"],
-		[{ list: "ordered" }, { list: "ordered" }],
-		[{ align: [] }],
-		["clean"],
-	];
+		uploadDocumentToWNFS(doc);
+	}
 
-	onMount(async () => {
-		const { default: Quill } = await import("quill");
-
-		let quill = new Quill(editor, {
-			modules: {
-				toolbar: toolbarOptions,
-			},
-			theme: "bubble",
-			placeholder: "",
-		});
-	});
+	function getBase64(image) {
+		const reader = new FileReader();
+		reader.readAsDataURL(image);
+		reader.onload = (e) => {
+			preview = e.target.result;
+		};
+	}
 </script>
 
-<label
-	for="upload-file"
-	class="flex flex-col justify-center items-center  object-cover cursor-pointer"
->
-	<div
-		class="flex flex-col justify-center items-center pt-5 pb-6"
+<section class="flex flex-col space-y-2 container mx-auto lg:max-w-5xl">
+	<label
+		for="upload-file"
+		class="flex flex-col justify-center items-center object-cover cursor-pointer "
 	>
-		<p class="mt-4 mb-2 text-sm">
-			<span class="font-bold text-sm">Cover Photo</span>
-		</p>
-		<p class="text-xxs">SVG, PNG, JPG or GIF</p>
-	</div>
+		<div
+			class="flex flex-col justify-end items-end py-4 h-60 container"
+			style="background: no-repeat center url('{preview || '../Pix.gif'}')"
+		>
+			<div class="bg-gray-100 text-center p-4 hover:bg-gray-200">
+				<span class="font-bold text-sm">Cover Photo</span>
+			</div>
+		</div>
+		<input
+			bind:files
+			id="upload-file"
+			type="file"
+			multiple
+			accept="image/*"
+			class="hidden"
+			on:change={() => getBase64(files[0])}
+		/>
+	</label>
 	<input
-		bind:files
-		id="upload-file"
-		type="file"
-		multiple
-		accept="image/*"
-		class="hidden"
+		type="text"
+		bind:value={contentHeader}
+		class="container mx-auto text-gray-900 dark:text-gray-50 dark:bg-gray-800 bg-gray-100 focus:border-violet-600 border border-gray-600 text-3xl"
+		placeholder="Header"
 	/>
-</label>
-<div
-	class="editor-wrapper h-60 container mx-auto text-gray-900 dark:text-gray-50 focus:border-violet-600 border"
->
-	<div id="editor" bind:this={editor} />
-</div>
+	<div
+		contenteditable="true"
+		class="h-80 container mx-auto text-gray-900 dark:text-gray-50 focus:border-violet-600 border border-gray-600 p-4"
+		bind:innerHTML={contentText}
+		placeholder="Content..."
+	/>
 
-<style>
-	@import "https://cdn.quilljs.com/1.3.6/quill.bubble.css";
-</style>
+	<div class="mx-auto container">
+		<button
+			on:click={uploadDoc}
+			class="px-4 py-2 bg-gray-50 dark:bg-violet-600 dark:text-gray-900"
+			>Save</button
+		>
+	</div>
+
+	<style>
+		@import "https://cdn.quilljs.com/1.3.6/quill.bubble.css";
+	</style>
+</section>
--- src/routes/gallery/lib/gallery.ts
+++ src/routes/gallery/lib/gallery.ts
@@ -1,39 +1,48 @@
-import { get as getStore } from 'svelte/store'
-import * as wn from 'webnative'
-import type PublicFile from 'webnative/fs/v1/PublicFile'
-import type PrivateFile from 'webnative/fs/v1/PrivateFile'
-import { isFile } from 'webnative/fs/types/check'
+/* eslint-disable @typescript-eslint/quotes */
+/* eslint-disable @typescript-eslint/member-delimiter-style */
+/* eslint-disable @typescript-eslint/semi */
+import { get as getStore } from "svelte/store";
+import * as wn from "webnative";
+import type PublicFile from "webnative/fs/v1/PublicFile";
+import type PrivateFile from "webnative/fs/v1/PrivateFile";
+import { isFile } from "webnative/fs/types/check";
 
-import { filesystemStore } from '$src/stores'
-import { AREAS, cmsStore } from '$routes/gallery/stores'
-import { addNotification } from '$lib/notifications'
-import { fileToUint8Array } from '$lib/utils'
+import { filesystemStore } from "$src/stores";
+import { AREAS, cmsStore } from "$routes/gallery/stores";
+import { addNotification } from "$lib/notifications";
+import { fileToUint8Array } from "$lib/utils";
 
 export type Image = {
-  cid: string
-  ctime: number
-  name: string
-  private: boolean
-  size: number
-  src: string
-}
+  cid: string;
+  ctime: number;
+  name: string;
+  private: boolean;
+  size: number;
+  src: string;
+};
 
 export type Gallery = {
-  publicDocuments: Image[] | null
-  privateDocuments: Image[] | null
-  selectedArea: AREAS
-  loading: boolean
-}
+  publicDocuments: Image[] | null;
+  privateDocuments: Image[] | null;
+  selectedArea: AREAS;
+  loading: boolean;
+};
+
+export type ContentDoc = {
+  image: string;
+  header: string;
+  content: string;
+};
 
 type Link = {
-  size: number
-}
+  size: number;
+};
 
 export const GALLERY_DIRS = {
-  [ AREAS.PUBLIC ]: [ 'public', 'gallery' ],
-  [ AREAS.PRIVATE ]: [ 'private', 'gallery' ]
-}
-const FILE_SIZE_LIMIT = 20
+  [AREAS.PUBLIC]: ["public", "gallery"],
+  [AREAS.PRIVATE]: ["private", "gallery"],
+};
+const FILE_SIZE_LIMIT = 20;
 
 /**
  * Get images from the user's WNFS and construct the `src` value for the images
@@ -41,165 +50,206 @@ const FILE_SIZE_LIMIT = 20
 export const getImagesFromWNFS: () => Promise<void> = async () => {
   try {
     // Set loading: true on the cmsStore
-    cmsStore.update(store => ({ ...store, loading: true }))
+    cmsStore.update((store) => ({ ...store, loading: true }));
 
-    const { selectedArea } = getStore(cmsStore)
-    const isPrivate = selectedArea === AREAS.PRIVATE
-    const fs = getStore(filesystemStore)
+    const { selectedArea } = getStore(cmsStore);
+    const isPrivate = selectedArea === AREAS.PRIVATE;
+    const fs = getStore(filesystemStore);
 
     // Set path to either private or public gallery dir
-    const path = wn.path.directory(...GALLERY_DIRS[ selectedArea ])
+    const path = wn.path.directory(...GALLERY_DIRS[selectedArea]);
 
     // Get list of links for files in the gallery dir
-    const links = await fs.ls(path)
+    const links = await fs.ls(path);
 
     let images = await Promise.all(
-      Object.entries(links).map(async ([ name ]) => {
+      Object.entries(links).map(async ([name]) => {
         const file = await fs.get(
-          wn.path.file(...GALLERY_DIRS[ selectedArea ], `${name}`)
-        )
+          wn.path.file(...GALLERY_DIRS[selectedArea], `${name}`),
+        );
 
-        if (!isFile(file)) return null
+        if (!isFile(file)) return null;
 
         // The CID for private files is currently located in `file.header.content`,
         // whereas the CID for public files is located in `file.cid`
         const cid = isPrivate
           ? (file as PrivateFile).header.content.toString()
-          : (file as PublicFile).cid.toString()
+          : (file as PublicFile).cid.toString();
 
         // Create a blob to use as the image `src`
-        const blob = new Blob([ file.content ])
-        const src = URL.createObjectURL(blob)
+        const blob = new Blob([file.content]);
+        const src = URL.createObjectURL(blob);
 
         const ctime = isPrivate
           ? (file as PrivateFile).header.metadata.unixMeta.ctime
-          : (file as PublicFile).header.metadata.unixMeta.ctime
+          : (file as PublicFile).header.metadata.unixMeta.ctime;
 
         return {
           cid,
           ctime,
           name,
           private: isPrivate,
-          size: (links[ name ] as Link).size,
-          src
-        }
-      })
-    )
+          size: (links[name] as Link).size,
+          src,
+        };
+      }),
+    );
 
     // Sort images by ctime(created at date)
     // NOTE: this will eventually be controlled via the UI
-    images = images.filter(a => !!a)
-    images.sort((a, b) => b.ctime - a.ctime)
+    images = images.filter((a) => !!a);
+    images.sort((a, b) => b.ctime - a.ctime);
 
     // Push images to the cmsStore
-    cmsStore.update(store => ({
+    cmsStore.update((store) => ({
       ...store,
       ...(isPrivate
         ? {
-          privateDocuments: images
+          privateDocuments: images,
         }
         : {
-          publicDocuments: images
+          publicDocuments: images,
         }),
-      loading: false
-    }))
+      loading: false,
+    }));
   } catch (error) {
-    console.error(error)
-    cmsStore.update(store => ({
+    console.error(error);
+    cmsStore.update((store) => ({
       ...store,
-      loading: false
-    }))
+      loading: false,
+    }));
   }
-}
+};
+
+/**
+ * Upload an image to the user's private or public WNFS
+ * @param image
+ */
+export const uploadDocumentToWNFS: (
+  doc: ContentDoc,
+) => Promise<string> = async (doc) => {
+  try {
+    console.log(doc);
+    const { selectedArea } = getStore(cmsStore);
+    const fs = getStore(filesystemStore);
+
+    // Reject the upload if the image already exists in the directory
+    const imageExists = await fs.exists(
+      wn.path.file(...GALLERY_DIRS[selectedArea], doc.header),
+    );
+    if (imageExists) {
+      throw new Error(`${doc.header} image already exists`);
+    }
+
+    // Create a sub directory and add some content
+    await fs.write(
+      wn.path.file(...GALLERY_DIRS[selectedArea], doc.header),
+      new TextEncoder().encode(JSON.stringify(doc)),
+    );
+
+    // Announce the changes to the server
+    await fs.publish();
+
+    addNotification(`${doc.header} image has been published`, "success");
+
+    return String(doc.header);
+  } catch (error) {
+    addNotification(error.message, "error");
+    console.error(error);
+  }
+};
 
 /**
  * Upload an image to the user's private or public WNFS
  * @param image
  */
 export const uploadImageToWNFS: (
-  image: File
-) => Promise<void> = async image => {
+  image: File,
+) => Promise<string> = async (image) => {
   try {
-    const { selectedArea } = getStore(cmsStore)
-    const fs = getStore(filesystemStore)
+    //console.log(image)
+    const { selectedArea } = getStore(cmsStore);
+    const fs = getStore(filesystemStore);
 
     // Reject files over 20MB
-    const imageSizeInMB = image.size / (1024 * 1024)
+    const imageSizeInMB = image.size / (1024 * 1024);
     if (imageSizeInMB > FILE_SIZE_LIMIT) {
-      throw new Error('Image can be no larger than 20MB')
+      throw new Error("Image can be no larger than 20MB");
     }
 
     // Reject the upload if the image already exists in the directory
     const imageExists = await fs.exists(
-      wn.path.file(...GALLERY_DIRS[ selectedArea ], image.name)
-    )
+      wn.path.file(...GALLERY_DIRS[selectedArea], image.name),
+    );
     if (imageExists) {
-      throw new Error(`${image.name} image already exists`)
+      throw new Error(`${image.name} image already exists`);
     }
 
     // Create a sub directory and add some content
     await fs.write(
-      wn.path.file(...GALLERY_DIRS[ selectedArea ], image.name),
-      await fileToUint8Array(image)
-    )
+      wn.path.file(...GALLERY_DIRS[selectedArea], image.name),
+      await fileToUint8Array(image),
+    );
 
     // Announce the changes to the server
-    await fs.publish()
+    await fs.publish();
 
-    addNotification(`${image.name} image has been published`, 'success')
+    addNotification(`${image.name} image has been published`, "success");
+
+    return String(image.name);
   } catch (error) {
-    addNotification(error.message, 'error')
-    console.error(error)
+    addNotification(error.message, "error");
+    console.error(error);
   }
-}
+};
 
 /**
  * Delete an image from the user's private or public WNFS
  * @param name
  */
 export const deleteImageFromWNFS: (
-  name: string
-) => Promise<void> = async name => {
+  name: string,
+) => Promise<void> = async (name) => {
   try {
-    const { selectedArea } = getStore(cmsStore)
-    const fs = getStore(filesystemStore)
+    const { selectedArea } = getStore(cmsStore);
+    const fs = getStore(filesystemStore);
 
     const imageExists = await fs.exists(
-      wn.path.file(...GALLERY_DIRS[ selectedArea ], name)
-    )
+      wn.path.file(...GALLERY_DIRS[selectedArea], name),
+    );
 
     if (imageExists) {
       // Remove images from server
-      await fs.rm(wn.path.file(...GALLERY_DIRS[ selectedArea ], name))
+      await fs.rm(wn.path.file(...GALLERY_DIRS[selectedArea], name));
 
       // Announce the changes to the server
-      await fs.publish()
+      await fs.publish();
 
-      addNotification(`${name} image has been deleted`, 'success')
+      addNotification(`${name} image has been deleted`, "success");
 
       // Refetch images and update cmsStore
-      await getImagesFromWNFS()
+      await getImagesFromWNFS();
     } else {
-      throw new Error(`${name} image has already been deleted`)
+      throw new Error(`${name} image has already been deleted`);
     }
   } catch (error) {
-    addNotification(error.message, 'error')
-    console.error(error)
+    addNotification(error.message, "error");
+    console.error(error);
   }
-}
+};
 
 /**
  * Handle uploads made by interacting with the file input directly
  */
 export const handleFileInput: (
-  files: FileList
-) => Promise<void> = async files => {
+  files: FileList,
+) => Promise<void> = async (files) => {
   await Promise.all(
-    Array.from(files).map(async file => {
-      await uploadImageToWNFS(file)
-    })
-  )
+    Array.from(files).map(async (file) => {
+      await uploadImageToWNFS(file);
+    }),
+  );
 
   // Refetch images and update cmsStore
-  await getImagesFromWNFS()
-}
+  await getImagesFromWNFS();
+};
