aadf40c558c434c155743a075f5a8d382f86e93b
Adam Sobotka <vorcigernix@gmail.com>
2023-03-24 10:37:20 +0100

    chatbot

--- src/components/editor/TipTap.svelte
+++ src/components/editor/TipTap.svelte
@@ -37,7 +37,7 @@
         //console.log(editorcontent)
       }
     });
-    editor.commands.setContent(`${editorcontent}`, false);
+    editor.commands.setContent(`${editorcontent}<p></p>`, false);
   });
 
   onDestroy(() => {
@@ -288,14 +288,17 @@
         class="editor-btn-primary"
       >
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
           class="h-5 w-5 text-stone-900 dark:text-stone-50"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
         >
-          <path fill="none" d="M0 0h24v24H0z" />
           <path
-            fill="currentColor"
-            d="M10 6v2H5v11h11v-5h2v6a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h6zm11-3v8h-2V6.413l-7.793 7.794-1.414-1.414L17.585 5H13V3h8z"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
           />
         </svg>
       </button>
